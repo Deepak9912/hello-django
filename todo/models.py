62fa5aa138e39e45cb973a6401f7666d954000e3
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-05-08 10:43:18 +0000

    update migrations for todo file

--- todo/models.py
+++ todo/models.py
@@ -7,5 +7,7 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
+    # to update the name of the new item as without this it give a,
+    # standard object name to our item
     def __str__(self):
         return self.name
